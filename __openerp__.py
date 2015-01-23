f513e372678b6c8a7ebc0a45a563750900145f20
adi <adiindc@gmail.com>
2015-01-23 15:22:04 -0800

    test
    tt

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Zebra Product Labels',
+    'version': '1.1',
+    'author': 'snippetbucket',
+    'category': 'Sales Management',
+    'depends': ['product'],    
+    'description': """
+            Module helps to print product label for zebra printer.
+            Its easily customizable and easy to integrate.
+            Snippetbucket.com
+    """,
+    'data': ['product_report.xml'],
+    'installable': True,
+    'auto_install': False,    
+}
